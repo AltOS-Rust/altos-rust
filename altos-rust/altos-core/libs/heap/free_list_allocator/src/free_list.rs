bf5d020c852c1c460347542ae5f3b2600bd0289b
Patrick Douglas <pcdouglas94@gmail.com>
2017-02-21 17:56:17 -0800

    Refactored allocation and deallocation code for list, adjusted shift_block_forward function.

--- altos-rust/altos-core/libs/heap/free_list_allocator/src/free_list.rs
+++ altos-rust/altos-core/libs/heap/free_list_allocator/src/free_list.rs
@@ -81,70 +81,87 @@ impl FreeList {
         self.block_hdr_size
     }
 
+    // Traverses the free list, looking for two sequential nodes which return true
+    // when passed into match_condition.
+    fn find_block(&mut self, match_condition: &Fn(*mut BlockHeader, *mut BlockHeader) -> bool)
+        -> (*mut BlockHeader, *mut BlockHeader) {
+
+        let (mut previous, mut current) = (ptr::null_mut(), self.head);
+        while !current.is_null() {
+            if !match_condition(previous, current) {
+                previous = current;
+                current = unsafe { (*current).next_block };
+                continue;
+            }
+            break;
+        }
+        (previous, current)
+    }
+
     // Allocate memory using first fit strategy
     // Returns pointer to allocated memory, or null if no memory is remaining
     pub fn allocate(&mut self, request_size: usize, request_align: usize) -> *mut u8 {
-
         let mut alloc_location: *mut u8 = ptr::null_mut();
         let using_size = alignment::align_up(request_size, self.block_hdr_size);
         let using_align = alignment::use_align(request_align, self.block_hdr_size);
 
-        unsafe {
-            let (mut previous, mut current) = (ptr::null_mut(), self.head);
-            while !current.is_null() {
-                let alloc_start = alignment::align_up(current as usize, using_align);
-                let align_diff = alloc_start - current as usize;
-                let current_size = (*current).block_size;
-                // Due to alignment, we should never get a case
-                // where 0 < remaining_size < block_size
+        // Will return true if current size is large enough to accomodate request
+        let acceptable_block = |previous: *mut BlockHeader, current: *mut BlockHeader| {
+            let alloc_start = alignment::align_up(current as usize, using_align);
+            let align_diff = alloc_start - current as usize;
+            let current_size = unsafe { (*current).block_size };
 
-                // Block does not have enough space to satisfy requirements of aligment and size
-                if current_size < using_size || current_size - align_diff < using_size {
-                    previous = current;
-                    // If current is null, this will not work!
-                    current = (*current).next_block;
-                    continue;
-                }
-                // Block is correct size and alignment.
-                else if current_size == using_size && align_diff == 0 {
-                    // If at head, there is no previous to adjust
-                    if self.head == current {
-                        self.head = (*self.head).next_block;
-                    }
-                    else {
-                        (*previous).next_block = (*current).next_block;
-                    }
-                }
-                // Current block is larger than required and has the correct alignment
-                else if align_diff == 0 {
-                    (*current).block_size -= using_size;
-                    if self.head == current {
-                        self.head = self.shift_block_forward(current, using_size);
-                    }
-                    else {
-                        (*previous).next_block = self.shift_block_forward(current, using_size);
-                    }
-                }
-                // Current block is larger than required but has exactly the right size to
-                // accomodate the alignment and size requirements
-                else if current_size == using_size + align_diff {
-                    (*current).block_size = align_diff;
-                }
-                // Current block is larger than required and because of alignment, the allocation
-                // divides it in two.
-                else {
-                    (*current).block_size = current_size - using_size - align_diff;
-                    let upper_block = self.shift_block_forward(current, using_size + align_diff);
-                    ptr::write(current as *mut BlockHeader, BlockHeader::new(align_diff));
-                    (*current).next_block = upper_block;
+            if current_size < using_size || current_size - align_diff < using_size { false }
+                else { true }
+        };
 
-                }
-                alloc_location = alloc_start as *mut u8;
-                break;
-            }
+        let (previous, current) = self.find_block(&acceptable_block);
+        if current.is_null() {
+            return current as *mut u8;
         }
 
-        alloc_location
+        let alloc_start = alignment::align_up(current as usize, using_align);
+        let align_diff = alloc_start - current as usize;
+        let current_size = unsafe { (*current).block_size };
+
+        // Block is correct size and alignment.
+        if current_size == using_size && align_diff == 0 {
+            // If at head, there is no previous to adjust
+            if self.head == current {
+                self.head = unsafe { (*self.head).next_block };
+            }
+            else {
+                unsafe { (*previous).next_block = (*current).next_block; }
+            }
+        }
+        // Current block is larger than required and has the correct alignment
+        else if align_diff == 0 {
+            unsafe { (*current).block_size -= using_size; }
+            if self.head == current {
+                self.head = self.shift_block_forward(current, using_size);
+            }
+            else {
+                unsafe {
+                    (*previous).next_block = self.shift_block_forward(current, using_size);
+                }
+            }
+        }
+        // Current block is larger than required but has exactly the right size to
+        // accomodate the alignment and size requirements
+        else if current_size == using_size + align_diff {
+            unsafe { (*current).block_size = align_diff; }
+        }
+        // Current block is larger than required and because of alignment, the allocation
+        // divides it in two.
+        else {
+            unsafe {
+                (*current).block_size = current_size - using_size - align_diff;
+                let upper_block = self.shift_block_forward(current, using_size + align_diff);
+                ptr::write(current as *mut BlockHeader, BlockHeader::new(align_diff));
+                (*current).next_block = upper_block;
+            }
+        }
+        alloc_start as *mut u8
     }
 
     /*
@@ -156,42 +173,39 @@ impl FreeList {
     */
 
     pub fn deallocate(&mut self, alloc_ptr: *mut u8, size: usize) {
+        // We can immediately add the block at the deallocated position
+        let alloc_block_ptr = alloc_ptr as *mut BlockHeader;
+        let used_memory = alignment::align_up(size, self.block_hdr_size);
         unsafe {
-            // We can immediately add the block at the deallocated position
-            let alloc_block_ptr = alloc_ptr as *mut BlockHeader;
-            let used_memory = alignment::align_up(size, self.block_hdr_size);
             ptr::write(&mut *alloc_block_ptr, BlockHeader::new(used_memory));
+        }
 
-            let (mut previous, mut current) = (ptr::null_mut(), self.head);
-            // Memory location to be added at head of the list
-            if alloc_block_ptr < current {
-                (*alloc_block_ptr).next_block = current;
-                self.head = alloc_block_ptr;
-                return;
-            }
-            while !current.is_null() {
-                // Will this comparison work the way I expect? Comparing pointer positions.
-                if previous < alloc_block_ptr && alloc_block_ptr < current {
-                    (*previous).next_block = alloc_block_ptr;
-                    (*alloc_block_ptr).next_block = current;
-                    return;
-                }
-                previous = current;
-                current = (*current).next_block;
-            }
-            // At this point, we know that it needs to be added at the end
+        // Memory location to be added at head of the list
+        if alloc_block_ptr < self.head {
+            unsafe { (*alloc_block_ptr).next_block = self.head; }
+            self.head = alloc_block_ptr;
+            return;
+        }
+        let (mut previous, current) = {
+            let free_mem_between = |previous, current| {
+                previous < alloc_block_ptr && alloc_block_ptr < current
+            };
+            self.find_block(&free_mem_between)
+        };
+        unsafe {
             (*previous).next_block = alloc_block_ptr;
+            (*alloc_block_ptr).next_block = current;
         }
     }
 
     // This relocates BlockHeaders in memory, used when we do allocations.
     fn shift_block_forward(&self, current_pos: *mut BlockHeader, offset_val: usize) -> *mut BlockHeader {
+        // If we don't convert this, offset does not work correctly
+        let current_ptr = current_pos as *mut u8;
         unsafe {
-            // If we don't convert this, offset does not work correctly
-            let current_ptr = current_pos as *mut u8;
             let new_pos = current_ptr.offset(offset_val as isize) as *mut BlockHeader;
             let current_block = ptr::read(current_pos);
-            mem::forget(mem::replace(&mut *new_pos, current_block));
+            *new_pos = current_block;
             new_pos as *mut BlockHeader
         }
     }
